a9da964b1d9dd127348471f436584b5709083ef4
Henry09Ramos <henryjeremiasramos09@gmail.com>
2025-12-23 11:05:10 -0600

    correction

--- pdf/dte03.pdf.ts
+++ pdf/dte03.pdf.ts
@@ -437,12 +437,10 @@ export const footerDocument = (doc: jsPDF, rectMargin: number, ccf: DteCcf) => {
     propina.toFixed(2),
     "0.00",
     resumen.totalPagar.toFixed(2),
-
-    resumen.tributos ? resumen.tributos.filter(item => item.codigo === "59")
-    .map(tr => Number(tr.valor))
-    .reduce((a, b) => a + b, 0) 
-    .toFixed(2)
-: "0.00"
+   resumen.tributos ? resumen.tributos
+      .filter((item) => item.codigo === "59")
+      .reduce((a, b) => a + Number(b.valor ?? 0), 0)
+      .toFixed(2) :'0.00',
 
   ];
 
