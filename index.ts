c6784ffc6732d3f15581160ea639aed88d150419
Carlos Daniel Contreras Hernandez <contrerasdannniel@gmail.com>
2026-05-23 12:05:08 -0600

    remove test

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 
-import JSON from "./7D89B81D-A898-418F-A7CA-182F58B17485_document.json"
-// import { generateSvfe01} from './pdf/dte01.pdf'
-// import { DteFe } from "./interfaces/dte01";
-import { DteCcf, generateSvfe03 } from "./main";
+// import JSON from "./7D89B81D-A898-418F-A7CA-182F58B17485_document.json"
+// // import { generateSvfe01} from './pdf/dte01.pdf'
+// // import { DteFe } from "./interfaces/dte01";
+// import { DteCcf, generateSvfe03 } from "./main";
 
-const getPdf = async () => {
-    // const pdf = await generateSvfe01(JSON as DteFe,"","",false,false,true,false,true)
+// const getPdf = async () => {
+//     // const pdf = await generateSvfe01(JSON as DteFe,"","",false,false,true,false,true)
 
-    const pdf = await generateSvfe03(JSON as unknown as DteCcf, "", "", false, false)
-    require("fs").writeFileSync("prueba.pdf", Buffer.from(pdf));
-}
+//     const pdf = await generateSvfe03(JSON as unknown as DteCcf, "", "", false, false)
+//     require("fs").writeFileSync("prueba.pdf", Buffer.from(pdf));
+// }
 
-getPdf();
+// getPdf();
