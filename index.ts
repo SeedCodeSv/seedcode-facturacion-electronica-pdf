259190119206aabc1b1bc05f72f17f25557d1ac6
Carlos Daniel Contreras Hernandez <contrerasdannniel@gmail.com>
2026-05-26 09:48:22 -0600

    logo whatsapp

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 
-import JSON from "./7D89B81D-A898-418F-A7CA-182F58B17485_document.json"
-import { generateQuoteAdvance01 } from "./pdf/quotes/advance/quote.advance.01.pdf";
-import { generateSvfe01} from './pdf/dte01.pdf'
-import { DteFe } from "./interfaces/dte01";
-// import { DteCcf, generateQuoteNormal, generateSvfe03 } from "./main";
-import quote from './quote.json'
-import { DteCcf, generateSvfe03 } from "./main";
+// import JSON from "./7D89B81D-A898-418F-A7CA-182F58B17485_document.json"
+// import { generateQuoteAdvance01 } from "./pdf/quotes/advance/quote.advance.01.pdf";
+// import { generateSvfe01} from './pdf/dte01.pdf'
+// import { DteFe } from "./interfaces/dte01";
+// // import { DteCcf, generateQuoteNormal, generateSvfe03 } from "./main";
+// import quote from './quote.json'
+// import { DteCcf, generateSvfe03 } from "./main";
 
-const getPdf = async () => {
-    const pdf = await generateSvfe03(JSON as unknown as DteCcf,"","",false,false)
+// const getPdf = async () => {
+//     const pdf = await generateSvfe03(JSON as unknown as DteCcf,"","",false,false)
 
-    // const pdf = await generateQuoteAdvance01(quote)
-    require("fs").writeFileSync("prueba.pdf", Buffer.from(pdf));
-}
+//     // const pdf = await generateQuoteAdvance01(quote)
+//     require("fs").writeFileSync("prueba.pdf", Buffer.from(pdf));
+// }
 
-getPdf();
+// getPdf();
